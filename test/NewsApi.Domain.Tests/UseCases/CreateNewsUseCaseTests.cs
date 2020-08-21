1a00679775f1e0bb0c3f9f83046ba21a28810eef
Gilmar <gilmardealcantara@gmail.com>
2020-08-21 00:41:45 -0300

    Add test for error flow in create news usecase

--- test/NewsApi.Domain.Tests/UseCases/CreateNewsUseCaseTests.cs
+++ test/NewsApi.Domain.Tests/UseCases/CreateNewsUseCaseTests.cs
@@ -32,9 +32,59 @@ namespace NewsApi.Domain.Tests.UseCases
             var response = await useCase.Execute(request);
 
             //Then
-            response.Success().Should().Be(true);
+            response.Success().Should().BeTrue();
             response.Result?.Title.Should().Be("My News");
             response.Result?.Id.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task UseCase_WhenRequestIsInvalid_ReturnValidateError()
+        {
+            //Given
+            var request = new CreateNewsRequest { Title = "My News" };
+            var logger = new Mock<ILogger<CreateNewsUseCase>>().Object;
+
+            var validatorMock = new Mock<IValidator<CreateNewsRequest>>();
+            validatorMock.Setup(v => v.Validate(request))
+            .Returns(new ValidationResult(new ValidationFailure[] {
+                new ValidationFailure("mockPropName", "mockMessage") {ErrorCode = "mockCode"}
+            }));
+
+            var repositoryMock = new Mock<INewsRepository>();
+
+            //When
+            var useCase = new CreateNewsUseCase(logger, validatorMock.Object, repositoryMock.Object);
+            var response = await useCase.Execute(request);
+
+            //Then
+            response.Success().Should().BeFalse();
+            response.Errors
+                .Should().NotBeNullOrEmpty()
+                .And.ContainSingle(x => x.Code == "mockCode");
+        }
+
+        [Fact]
+        public async Task UseCase_WhenRepositoryThrowsAnException_Returnerror()
+        {
+            //Given
+            var request = new CreateNewsRequest { Title = "My News" };
+            var logger = new Mock<ILogger<CreateNewsUseCase>>().Object;
+
+            var validatorMock = new Mock<IValidator<CreateNewsRequest>>();
+            validatorMock.Setup(v => v.Validate(request)).Returns(new ValidationResult());
+
+            var repositoryMock = new Mock<INewsRepository>();
+            repositoryMock.Setup(r => r.Save(It.IsAny<News>())).Throws(new System.Exception());
+
+            //When
+            var useCase = new CreateNewsUseCase(logger, validatorMock.Object, repositoryMock.Object);
+            var response = await useCase.Execute(request);
+
+            //Then
+            response.Success().Should().BeFalse();
+            response.Errors
+                .Should().NotBeNullOrEmpty()
+                .And.ContainSingle(x => x.Code == "01");
         }
     }
 }
