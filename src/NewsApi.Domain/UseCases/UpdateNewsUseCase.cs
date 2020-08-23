f6ea1bd959bd8a513142416fbdb2c01280c086a9
Gilmar <gilmardealcantara@gmail.com>
2020-08-23 02:42:34 -0300

    update thumbnail in update news

--- src/NewsApi.Domain/UseCases/UpdateNewsUseCase.cs
+++ src/NewsApi.Domain/UseCases/UpdateNewsUseCase.cs
@@ -3,13 +3,14 @@ using System.Threading.Tasks;
 using FluentValidation;
 using Microsoft.Extensions.Logging;
 using NewsApi.Domain.Dtos;
+using NewsApi.Domain.Entities;
 using NewsApi.Domain.Services.ExternalApis;
 using NewsApi.Domain.Services.Repositories;
 using NewsApi.Domain.Shared;
 
 namespace NewsApi.Domain.UseCases
 {
-    public class UpdateNewsUseCase : UseCaseBase<UpdateNewsRequest, Guid>
+    public class UpdateNewsUseCase : UseCaseBase<UpdateNewsRequest, News>
     {
         INewsRepository _repository;
         IImageStorageService _imageStorage;
@@ -22,25 +23,24 @@ namespace NewsApi.Domain.UseCases
             : base(logger, validator, ("03", "Error unexpected when update news"))
            => (_repository, _imageStorage) = (repository, imageStorage);
 
-        protected override async Task<UseCaseResponse<Guid>> OnExecute(UpdateNewsRequest request)
+        protected override async Task<UseCaseResponse<News>> OnExecute(UpdateNewsRequest request)
         {
-            var news = await _repository.GetById(request.Id);
-            if (news is null)
+            var oldNews = await _repository.GetById(request.Id);
+            if (oldNews is null)
                 return _response.SetResourceNotFountError();
 
-            var updatedNews = request.ToNews(news);
+            string thumbnailRemoteURL = null;
+            if (oldNews.ThumbnailURL is null && request.ThumbnailLocalURL != null)
+                thumbnailRemoteURL = await _imageStorage.Upload(oldNews.Id.ToString(), request.ThumbnailLocalURL);
 
-            string thumbnailRemoteURL = "";
-            if (news.ThumbnailURL is null && request.ThumbnailLocalURL != null)
-                thumbnailRemoteURL = await _imageStorage.Upload(news.Id.ToString(), request.ThumbnailLocalURL);
+            if (oldNews.ThumbnailURL != null && request.ThumbnailLocalURL != null)
+                thumbnailRemoteURL = await _imageStorage.Update(oldNews.Id.ToString(), request.ThumbnailLocalURL);
 
-            if (news.ThumbnailURL != null && request.ThumbnailLocalURL != null)
-                thumbnailRemoteURL = await _imageStorage.Update(news.Id.ToString(), request.ThumbnailLocalURL);
-
-            news.ThumbnailURL = thumbnailRemoteURL;
+            var updatedNews = request.ToNews(oldNews);
+            updatedNews.ThumbnailURL = thumbnailRemoteURL ?? oldNews.ThumbnailURL;
 
             await _repository.Update(updatedNews);
-            return _response.SetResult(updatedNews.Id);
+            return _response.SetResult(updatedNews);
         }
     }
 }
